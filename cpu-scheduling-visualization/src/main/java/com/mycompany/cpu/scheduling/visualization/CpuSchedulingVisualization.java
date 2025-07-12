c77c6d786827273412bd780298d46024039e0295
annie <annie.mamitag25@gmail.com>
2025-07-12 14:53:36 +0800

    Initial commit: basic GUI scheduler setup

---
+++ cpu-scheduling-visualization/src/main/java/com/mycompany/cpu/scheduling/visualization/CpuSchedulingVisualization.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cpu.scheduling.visualization;
+
+/**
+ *
+ * @author annie
+ */
+public class CpuSchedulingVisualization {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
